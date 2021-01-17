de1524a43ac93e1943ccda4ec7435ca99146c7f2
aorfevre <aorfevre@gmail.com>
2021-01-17 21:36:33 +0100

    Creating auth

--- custo/init.js
+++ custo/init.js
@@ -18,13 +18,45 @@ module.exports.setTelegram = function() {
   var telegramToken = BOT_TOKEN_TELEGRAM_PROD
   global.isDev = __dirname.indexOf("aorfevre") !== -1
 
-  return new TelegramBot(telegramToken, {
-    polling: {
-      interval: 200,
-      limit: 75,
-      autoStart: true,
-      allowed_updates: ["message", "inline_query", "callback_query"]
-    }
-  });
+
+  var bot = null;
+  if (!isDev) {
+    // if (false) {
+    const options = {
+
+      webHook: {
+        // Just use 443 directly
+        port: 443
+      }
+    };
+    var url = 'https://tradcommas-api-dwja7wuiva-ew.a.run.app'
+    // url = 'https://api.telegram.org'
+    bot = new TelegramBot(telegramToken, options);
+    bot.setWebHook(`${url}/bot${telegramToken}`);
+
+    bot.on('webhook_error', (error) => {
+      console.log("Webhook error", error.code, error); // => 'EPARSE'
+    });
+  } else {
+    const options = {
+      polling: {
+        autoStart: true,
+        allowed_updates: ["message", "inline_query", "callback_query"]
+      },
+
+    };
+    bot = new TelegramBot(telegramToken, {
+      polling: true
+    });
+  }
+  return bot;
+  // return new TelegramBot(telegramToken, {
+  //   polling: {
+  //     interval: 200,
+  //     limit: 75,
+  //     autoStart: true,
+  //     allowed_updates: ["message", "inline_query", "callback_query"]
+  //   }
+  // });
 
 }
